5e5b68f99c6cb9a65d2fa13972810b98da70ccfb
ElRaptorus <christian.werner@5minds.de>
2019-01-16 11:39:31 +0100

    :sparkles: Add userId to identity type

--- src/identity.ts
+++ src/identity.ts
@@ -1,9 +1,12 @@
 import {IIdentity} from '@essential-projects/iam_contracts';
 
 export class Identity implements IIdentity {
-  public token: string;
 
-  constructor(token: string) {
+  public readonly token: string;
+  public readonly userId: string;
+
+  constructor(token: string, userId: string) {
     this.token = token;
+    this.userId = userId;
   }
 }
