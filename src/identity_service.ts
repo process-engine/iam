98d9497f0032b95deba7d88d77d150d3be50a1ca
ElRaptorus <christian.werner@5minds.de>
2019-01-16 11:41:27 +0100

    :recycle: Use EssentialProject errors in identity service

--- src/identity_service.ts
+++ src/identity_service.ts
@@ -1,3 +1,4 @@
+import {BadRequestError} from '@essential-projects/errors_ts';
 import {IIdentity, IIdentityService} from '@essential-projects/iam_contracts';
 
 import {Identity} from './identity';
@@ -7,7 +8,7 @@ export class IdentityService implements IIdentityService {
   public getIdentity(token: string): Promise<IIdentity> {
 
     if (token === null) {
-      throw new Error('Cannot get Identity by token');
+      throw new BadRequestError('Must provide a token by which to create an identity!');
     }
 
     const identity: IIdentity = new Identity(token);
