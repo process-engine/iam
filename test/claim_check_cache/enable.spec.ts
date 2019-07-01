340d656f0e142114e29ac7ca6a173624783f0672
ElRaptorus <christian.werner@5minds.de>
2019-07-01 15:42:24 +0200

    :white_check_mark: Add tests for enabling the cache

---
+++ test/claim_check_cache/enable.spec.ts
@@ -0,0 +1,52 @@
+// Note: The tests are accessing private variables, so we must use this type of notation.
+/* eslint-disable dot-notation */
+import * as should from 'should';
+
+import {ClaimCheckCache} from '../../dist/commonjs/claim_check_cache';
+
+describe('ClaimCheckCache.enable()', (): void => {
+
+  let testCache;
+
+  before((): void => {
+    const configToUse = {
+      enabled: false,
+      cleanupIntervalInSeconds: 1,
+    };
+
+    testCache = new ClaimCheckCache(configToUse);
+  });
+
+  afterEach((): void => {
+    testCache.disable();
+  });
+
+  it('Should create and start a timer that periodically calls removeOutdatedEntries', async (): Promise<void> => {
+
+    return new Promise((resolve, reject): void => {
+      testCache.removeOutdatedEntries = (): void => {
+        should(testCache.enabled).be.true();
+
+        testCache.disable();
+
+        resolve();
+      };
+
+      testCache.enable();
+    });
+  });
+
+  it('Should set the "enabled" flag to "true"', (): void => {
+    testCache.enable();
+    should(testCache.enabled).be.true();
+  });
+
+  it('Should ignore repeated calls, when the cache is already enabled', (): void => {
+    testCache.enable();
+    testCache.enable();
+    testCache.enable();
+    testCache.enable();
+    should(testCache.enabled).be.true();
+  });
+
+});
