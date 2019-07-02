07a472dae0a7b9b164e1f7256ba5f32b0daa8513
ElRaptorus <christian.werner@5minds.de>
2019-07-02 14:44:46 +0200

    :white_check_mark: Add tests for permanent cache

--- test/claim_check_cache/enable.spec.ts
+++ test/claim_check_cache/enable.spec.ts
@@ -6,43 +6,117 @@ import {ClaimCheckCache} from '../../dist/commonjs/claim_check_cache';
 
 describe('ClaimCheckCache.enable()', (): void => {
 
-  let testCache;
+  describe('Startup', (): void => {
 
-  before((): void => {
-    const configToUse = {
-      enabled: false,
-      cleanupIntervalInSeconds: 1,
-    };
+    let testCache;
 
-    testCache = new ClaimCheckCache(configToUse);
-  });
-
-  afterEach((): void => {
-    testCache.disable();
-  });
-
-  it('Should create and start a timer that periodically calls removeOutdatedEntries', async (): Promise<void> => {
-
-    return new Promise((resolve, reject): void => {
-      testCache.removeOutdatedEntries = (): void => {
-        should(testCache.enabled).be.true();
-
-        resolve();
+    before((): void => {
+      const configToUse = {
+        enabled: false,
+        cleanupIntervalInSeconds: 1,
       };
 
+      testCache = new ClaimCheckCache(configToUse);
+    });
+
+    afterEach((): void => {
+      testCache.disable();
+    });
+
+    it('Should set the "enabled" flag to "true"', (): void => {
+      testCache.enable();
+      should(testCache.enabled).be.true();
+    });
+
+    it('Should ignore repeated calls, when the cache is already enabled', (): void => {
+      testCache.enable();
+      testCache.enable();
+      testCache.enable();
       testCache.enable();
     });
   });
 
-  it('Should set the "enabled" flag to "true"', (): void => {
-    testCache.enable();
-    should(testCache.enabled).be.true();
+  describe('Transient cache', (): void => {
+
+    let testCache;
+
+    before((): void => {
+      const configToUse = {
+        enabled: false,
+        cleanupIntervalInSeconds: 1,
+      };
+
+      testCache = new ClaimCheckCache(configToUse);
+    });
+
+    afterEach((): void => {
+      testCache.disable();
+    });
+
+    it('Should create and start a timer that periodically calls removeOutdatedEntries', async (): Promise<void> => {
+
+      return new Promise((resolve, reject): void => {
+        testCache.removeOutdatedEntries = (): void => {
+          should(testCache.enabled).be.true();
+
+          resolve();
+        };
+
+        testCache.enable();
+      });
+    });
   });
 
-  it('Should ignore repeated calls, when the cache is already enabled', (): void => {
-    testCache.enable();
-    testCache.enable();
-    testCache.enable();
-    testCache.enable();
+  describe('Permanent cache', (): void => {
+
+    it('Should not call removeOutdatedEntries periodically, when cleanup interval is set to 0', async (): Promise<void> => {
+
+      const configToUse = {
+        enabled: false,
+        cacheLifetimeInSeconds: 1,
+        cleanupIntervalInSeconds: 0,
+      };
+
+      const testCache = new ClaimCheckCache(configToUse);
+
+      let removeOutdatedEntriesCalled = false;
+
+      testCache.removeOutdatedEntries = (): void => {
+        removeOutdatedEntriesCalled = true;
+      };
+
+      testCache.enable();
+
+      await new Promise((resolve): void => { setTimeout(resolve, 1100); });
+
+      should(removeOutdatedEntriesCalled).be.false();
+
+      testCache.disable();
+    });
+
+    it('Should not call removeOutdatedEntries periodically, when cache value lifetime is set to 0', async (): Promise<void> => {
+
+      const configToUse = {
+        enabled: false,
+        cacheLifetimeInSeconds: 0,
+        cleanupIntervalInSeconds: 1,
+      };
+
+      const testCache = new ClaimCheckCache(configToUse);
+
+      let removeOutdatedEntriesCalled = false;
+
+      testCache.removeOutdatedEntries = (): void => {
+        removeOutdatedEntriesCalled = true;
+      };
+
+      testCache.enable();
+
+      await new Promise((resolve): void => { setTimeout(resolve, 1100); });
+
+      should(removeOutdatedEntriesCalled).be.false();
+
+      testCache.disable();
+    });
   });
 });
--- test/claim_check_cache/remove_outdated_entries.spec.ts
+++ test/claim_check_cache/remove_outdated_entries.spec.ts
@@ -7,63 +7,131 @@ import {ClaimCheckCache} from '../../dist/commonjs/claim_check_cache';
 
 describe('ClaimCheckCache.removeOutdatedEntries()', (): void => {
 
-  let testCache;
+  describe('Transient cache', (): void => {
 
-  before((): void => {
-    const configToUse = {
-      enabled: true,
-      cacheLifetimeInSeconds: 60,
-      cleanupIntervalInSeconds: 666,
-    };
+    let testCache;
 
-    testCache = new ClaimCheckCache(configToUse);
+    before((): void => {
+      const configToUse = {
+        enabled: true,
+        cacheLifetimeInSeconds: 60,
+        cleanupIntervalInSeconds: 666,
+      };
+
+      testCache = new ClaimCheckCache(configToUse);
+    });
+
+    after((): void => {
+      testCache.disable();
+    });
+
+    it('Should remove all values from the cache that are past the configured expiration time', (): void => {
+
+      const sampleCache = {
+        userId1: {
+          claim1: {
+            userHasClaim: true,
+            lastCheckedAt: moment().add(5, 'seconds'),
+          },
+          claim2: {
+            userHasClaim: false,
+            lastCheckedAt: moment().subtract(120, 'seconds'),
+          },
+        },
+        userId2: {
+          claim1: {
+            userHasClaim: true,
+            lastCheckedAt: moment().subtract(55, 'seconds'),
+          },
+          claim2: {
+            userHasClaim: false,
+            lastCheckedAt: moment(),
+          },
+          claim3: {
+            userHasClaim: false,
+            lastCheckedAt: moment().subtract(61, 'seconds'),
+          },
+        },
+      };
+
+      testCache['cache'] = sampleCache;
+
+      testCache.removeOutdatedEntries();
+
+      should.exist(testCache['cache']['userId1']);
+      should.exist(testCache['cache']['userId2']);
+
+      should.exist(testCache['cache']['userId1']['claim1']);
+      should.not.exist(testCache['cache']['userId1']['claim2']);
+
+      should.exist(testCache['cache']['userId2']['claim1']);
+      should.exist(testCache['cache']['userId2']['claim2']);
+      should.not.exist(testCache['cache']['userId2']['claim3']);
+    });
+
   });
 
-  after((): void => {
-    testCache.disable();
-  });
+  describe('Permanent cache', (): void => {
 
-  it('Should remove all values from the cache that are past the configured expiration time', (): void => {
+    let testCache;
 
-    const sampleCache = {
-      userId1: {
-        claim1: {
-          userHasClaim: true,
-          lastCheckedAt: moment().add(5, 'seconds'),
+    before((): void => {
+      const configToUse = {
+        enabled: true,
+        cacheLifetimeInSeconds: 0,
+        cleanupIntervalInSeconds: 1,
+      };
+
+      testCache = new ClaimCheckCache(configToUse);
+    });
+
+    after((): void => {
+      testCache.disable();
+    });
+
+    it('Should not remove any values from the cache, when the cache value lifetime is set to 0', (): void => {
+
+      const sampleCache = {
+        userId1: {
+          claim1: {
+            userHasClaim: true,
+            lastCheckedAt: moment().add(5, 'seconds'),
+          },
+          claim2: {
+            userHasClaim: false,
+            lastCheckedAt: moment().subtract(120, 'seconds'),
+          },
         },
-        claim2: {
-          userHasClaim: false,
-          lastCheckedAt: moment().subtract(120, 'seconds'),
+        userId2: {
+          claim1: {
+            userHasClaim: true,
+            lastCheckedAt: moment().subtract(55, 'seconds'),
+          },
+          claim2: {
+            userHasClaim: false,
+            lastCheckedAt: moment(),
+          },
+          claim3: {
+            userHasClaim: false,
+            lastCheckedAt: moment().subtract(61, 'seconds'),
+          },
         },
-      },
-      userId2: {
-        claim1: {
-          userHasClaim: true,
-          lastCheckedAt: moment().subtract(55, 'seconds'),
-        },
-        claim2: {
-          userHasClaim: false,
-          lastCheckedAt: moment(),
-        },
-        claim3: {
-          userHasClaim: false,
-          lastCheckedAt: moment().subtract(61, 'seconds'),
-        },
-      },
-    };
+      };
 
-    testCache['cache'] = sampleCache;
+      testCache['cache'] = sampleCache;
 
-    testCache.removeOutdatedEntries();
+      testCache.removeOutdatedEntries();
 
-    should.exist(testCache['cache']['userId1']);
-    should.exist(testCache['cache']['userId2']);
+      should.exist(testCache['cache']['userId1']);
+      should.exist(testCache['cache']['userId2']);
 
-    should.exist(testCache['cache']['userId1']['claim1']);
-    should.not.exist(testCache['cache']['userId1']['claim2']);
+      should.exist(testCache['cache']['userId1']['claim1']);
+      should.exist(testCache['cache']['userId1']['claim2']);
 
-    should.exist(testCache['cache']['userId2']['claim1']);
-    should.exist(testCache['cache']['userId2']['claim2']);
-    should.not.exist(testCache['cache']['userId2']['claim3']);
+      should.exist(testCache['cache']['userId2']['claim1']);
+      should.exist(testCache['cache']['userId2']['claim2']);
+      should.exist(testCache['cache']['userId2']['claim3']);
+    });
+
   });
 });
